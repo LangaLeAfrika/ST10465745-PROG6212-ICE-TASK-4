e3f3ea17df3d2d64187e3d7ba7ce134a6fb5329f
LangaLeAfrika <ST10465745@rcconnect.edu.za>
2026-09-16 15:19:26 +0200

    Initial commit

---
+++ ClaimContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ClaimDataManager
+{
+    public class ClaimContext : DbContext
+    {
+        public DbSet<Claim> Claims { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=claims.db");
+        }
+    }
+}
